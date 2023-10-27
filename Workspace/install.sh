d70e004cde3afe7d5c795d274fe5716ba0c1c3b7
Alex <3707177+skipidar@users.noreply.github.com>
2023-10-27 09:58:01 +0200

    fix node installation

--- Workspace/install.sh
+++ Workspace/install.sh
@@ -153,18 +153,23 @@ su - $USER
 NODE_MAJOR_VERSION="20"
 
 
+
+## SCRIPT DEPRECATION WARNING
 # node.js
 # add a source file for the official Node.js LTS repo, grab the signing key and will run apt update
-curl -sL https://deb.nodesource.com/setup_${NODE_MAJOR_VERSION}.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# curl -sL https://deb.nodesource.com/setup_${NODE_MAJOR_VERSION}.x | sudo -E bash -
+# sudo apt-get install -y nodejs
 
 
+# switch 
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
 
-
-
-
-
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR_VERSION.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 
 
